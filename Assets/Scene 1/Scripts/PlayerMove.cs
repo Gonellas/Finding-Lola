acea83521e4b3d6e27a070b5d23d5b7d2f937393
Gonellas <camilamgonella@gmail.com>
2023-05-30 22:29:41 -0300

    Character movement and dash

--- Assets/Scene 1/Scripts/PlayerMove.cs
+++ Assets/Scene 1/Scripts/PlayerMove.cs
@@ -2,17 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class PlayerMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Rigidbody2D RB2D;
+    public float dashForce;
+    public float speed = 3f;
+    public KeyCode dashKey;
+    float aXH, aXV;
+    
+    Vector2 direction;
+
+    private void Start()
     {
-        
+        RB2D = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+         aXH = Input.GetAxisRaw("Horizontal");
+         aXV = Input.GetAxisRaw("Vertical");
+
+        //Movement
+        //transform.position += (Vector3.right * aXH + Vector3.up * aXV) * speed * Time.deltaTime;
+
+        direction = new Vector2(aXH, aXV).normalized;           
+
+        //Dash
+        if (Input.GetKeyDown(dashKey) && direction != Vector2.zero)
+        {
+            RB2D.AddForce(direction * dashForce, ForceMode2D.Impulse);
+        }
+
+        Debug.Log(direction.magnitude);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
+        RB2D.MovePosition(new Vector3(RB2D.position.x, RB2D.position.y, 0) +
+            (Vector3.right * aXH + Vector3.up * aXV) * speed * Time.fixedDeltaTime);
+
         
     }
 }
