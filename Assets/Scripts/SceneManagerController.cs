0cd769b9d3300a766783cdcdda563fca851f1126
Gonellas <camilamgonella@gmail.com>
2023-07-03 03:32:01 -0300

    Menu
    Falta terminar cambio de escenas

---
+++ Assets/Scripts/SceneManagerController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerController : MonoBehaviour
+{
+    public GameObject[] allMenues;
+    public GameObject wantedActiveMenu;
+
+    private void Start()
+    {
+        //Si el menu activado es mayor a 0 (elemento 1 en la lista)
+        if(allMenues.Length > 0)
+        {
+            //Recorre la lista y desactiva los menues activados
+            foreach (GameObject menu in allMenues)
+            {
+                menu.SetActive(false);
+            }
+        }
+
+        //Si el menu principal no está activado, activalo
+        if (wantedActiveMenu != null) wantedActiveMenu.SetActive(true);
+    }
+
+    public void ChangeScene()
+    {
+        //Cambio de escena por index
+        SceneManager.LoadScene(1);
+    }
+    public void ExitGame()
+    {
+        //Si estamos en el editor, cerrá el juego con este código
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
