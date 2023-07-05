6f7784d97ac7b33703050d3989f3c4094f96acdb
Gonellas <camilamgonella@gmail.com>
2023-07-05 17:20:08 -0300

    codigo anim disparo arreglado

--- Assets/Scripts/PlayerAnims.cs
+++ Assets/Scripts/PlayerAnims.cs
@@ -15,7 +15,7 @@ public class PlayerAnims : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if (Input.GetKey(KeyCode.Mouse0))
         {
             playerAnim.SetTrigger("isShooting");
         }
