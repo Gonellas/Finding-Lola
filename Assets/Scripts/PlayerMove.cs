8a2f6f3c85160fb4b7c00f1a105bd960bdc7d986
Gonellas <camilamgonella@gmail.com>
2023-07-05 15:48:37 -0300

    dash andando

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -26,11 +26,18 @@ public class PlayerMove : MonoBehaviour
     }
     private void Update()
     {
-         aXH = Input.GetAxisRaw("Horizontal");
-         aXV = Input.GetAxisRaw("Vertical");
+        aXH = Input.GetAxisRaw("Horizontal");
+        aXV = Input.GetAxisRaw("Vertical");
 
-        //Movement
-        //transform.position += (Vector3.right * aXH + Vector3.up * aXV) * speed * Time.deltaTime;
+        direction = new Vector2(aXH, aXV).normalized;
+
+        //Dash
+        if (Input.GetKeyDown(dashKey) && direction != Vector2.zero)
+        {
+            RB2D.AddForce(direction * dashForce, ForceMode2D.Impulse);
+        }
+
+        Debug.Log("Dash" + Input.GetKeyDown(dashKey));
 
         //(Rotacion del auto)
         //transform.Rotate(aXH * Vector3.back * playerRotateSpeed * Time.deltaTime);
@@ -39,13 +46,6 @@ public class PlayerMove : MonoBehaviour
 
         //transform.position += transform.right * aXV * playerRotateSpeed * Time.deltaTime;
 
-        direction = new Vector2(aXH, aXV).normalized;
-        
-        //Dash
-        if (Input.GetKeyDown(dashKey) && direction != Vector2.zero)
-        {           
-            RB2D.AddForce(direction * dashForce, ForceMode2D.Impulse);
-        }
 
         ////Si timerCooldown es menor a damageCooldown, se suma un timerCooldown
         //if (timerCooldown < damageCooldown) timerCooldown += Time.deltaTime;
@@ -70,9 +70,11 @@ public class PlayerMove : MonoBehaviour
 
     private void FixedUpdate()
     {
-        RB2D.MovePosition((Vector3)RB2D.position + (Vector3.right * aXH + Vector3.up * aXV) * speed * Time.fixedDeltaTime);
-        //RB2D.AddForce(direction * speed, ForceMode2D.Force);
+        //Movement
+        transform.position += (Vector3.right * aXH + Vector3.up * aXV) * speed * Time.deltaTime;
 
+        //RB2D.MovePosition((Vector3)RB2D.position + (Vector3.right * aXH + Vector3.up * aXV) * speed * Time.fixedDeltaTime);
+        //RB2D.AddForce(direction * speed, ForceMode2D.Force);
     }
 
     //private void OnCollisionEnter2D(Collision2D collision)
