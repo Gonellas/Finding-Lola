32fdf706bb5983b33092dfa00507ad11a33f6434
Gonellas <camilamgonella@gmail.com>
2023-07-07 16:19:16 -0300

    arrow

--- Assets/Scripts/Healing.cs
+++ Assets/Scripts/Healing.cs
@@ -8,11 +8,22 @@ public class Healing : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.layer == 8)
+        //if (collision.gameObject.layer == 8)
+        //{
+        //    PlayerLife _playerLife = collision.gameObject.GetComponent<PlayerLife>();
+
+        //    if(_playerLife.playerLife < _playerLife.playerMaxLife) 
+        //    {
+        //        _playerLife.GetHeal(healingAmount);
+        //        Destroy(gameObject);
+        //    }
+        //}
+
+        if (collision.gameObject.CompareTag("Player") && Input.GetKey(KeyCode.E))
         {
             PlayerLife _playerLife = collision.gameObject.GetComponent<PlayerLife>();
 
-            if(_playerLife.playerLife < _playerLife.playerMaxLife) 
+            if (_playerLife.playerLife < _playerLife.playerMaxLife)
             {
                 _playerLife.GetHeal(healingAmount);
                 Destroy(gameObject);
@@ -20,3 +31,4 @@ public class Healing : MonoBehaviour
         }
     }
 }
+
