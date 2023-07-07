b2ecddc4d42cd46e28cc771f9a16bd782748d699
AnahiSavoia <97140747+AnahiSavoia@users.noreply.github.com>
2023-07-07 13:43:35 -0300

    Hecho el tuto
    con la curación de objetos. Falta arreglar un par de bug de colisiones y que desparezcan los objetos healing.

---
+++ Assets/Scripts/Healing.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Healing : MonoBehaviour
+{
+    public float healingAmount = 50f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 8)
+        {
+            PlayerLife _playerLife = collision.gameObject.GetComponent<PlayerLife>();
+
+            if(_playerLife.playerLife < _playerLife.playerMaxLife) 
+            {
+                _playerLife.GetHeal(healingAmount);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
